a6dae9bc1ffd020f323cfa1c2b0ba53086d8ef38
vianat <nik.sergey87@mail.ru>
2016-12-27 12:24:25 +0300

    th

---
+++ chapter001/src/main/ru/nikiforov/Calculate.java
@@ -0,0 +1,20 @@
+package src.main.ru.nikiforov;
+
+/**
+* cout ("hello")
+* @author nikiforov
+* @since 21.12.2016
+* @version 1.0
+*/
+
+public class Calculate{
+
+	/**
+	* Выводи на экран Hello world
+	*/
+
+	public static void main(String[] arg) {
+
+		System.out.println("Hello world!");
+	}
+}
