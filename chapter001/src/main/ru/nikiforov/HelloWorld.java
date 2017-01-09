8052d0226f405fe738f9ae8da84c758c6138ab07
vianat <nik.sergey87@mail.ru>
2017-01-09 15:39:29 +0300

    edit

--- chapter001/src/main/ru/nikiforov/HelloWorld.java
+++ chapter001/src/main/ru/nikiforov/HelloWorld.java
@@ -1,5 +1,10 @@
+/**
+* @author nikiforov
+* @since 21.12.2016
+* @version 1.0
+*/
 
-
+//класс печатает привет
 class HelloWorld {
     public static void main(String[] args) {
         // Display "Hello World!"
